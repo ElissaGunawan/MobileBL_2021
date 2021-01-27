40ef3420e429f1f5f1b91d3d6c4b535cfe9d9a7e
Your Name <you@example.com>
2021-01-27 15:56:23 +0700

    first commit

---
+++ Week01_00000027328/app/src/main/java/umn/ac/id/week01_00000027328/MainActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.week01_00000027328;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
